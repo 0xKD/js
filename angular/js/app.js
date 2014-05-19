43e32c6797a1d9483d2cf8dc91d06b42dffd21f5
Kedar <kedardeore@gmail.com>
2014-05-19 18:22:40 +0530

    angular basics update

--- angular/js/app.js
+++ angular/js/app.js
@@ -15,11 +15,19 @@ var MyController = function($scope) {
 
 // do not pollute global scope with functions, do this instead
 
+// first arg is name of module, second is dependencies
 var app = angular.module('app', []);
 
-app.controller('MyController', function($scope) {
+// declaring a controller
+// 
+// first arg is name of controller, 
+//  second arg is array whose last arg is function, rest of the args are 
+//  dependencies to the controller function passed to it as arguments,
+//  order of dependencies must match order of arguments to function.
+app.controller('MyController', ['$scope', function($scope) {
+	// add variables / functions to $scope
 	$scope.val = "King's Landing";
 	$scope.add = function() {
 		return 3 + 3;
 	}
-});
+}]);
