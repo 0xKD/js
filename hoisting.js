09b785c95352e67b82f9fd65bd4d55b42b029c3c
Kedar <kedardeore@gmail.com>
2014-10-11 23:22:15 +0530

    Added more details about the execution context w.r.t hoisting

--- hoisting.js
+++ hoisting.js
@@ -31,5 +31,37 @@ function test() {
 
 test();
 
-// always use var to declare variables, declare variables just once pers scope
+// always use var to declare variables, declare variables just once per scope
 //  at the top of the scope, to avoid hoisting related confusion
+
+
+// http://davidshariff.com/blog/what-is-the-execution-context-in-javascript/
+
+// more with regards to execution context
+
+(function() {
+	/*
+	when function is called, before it executes, an execution context is created
+	- property is created in the variable object which has reference to function
+	- variables are given value 'undefined'
+	- if variable already exists in variable object, it is ignored
+	 */
+
+	// these statements are encountered before everything else but after 
+	//  the execution context is defined
+	console.log(typeof foo); // function pointer
+	console.log(typeof bar); // undefined
+
+	// foo is already in the execution context, so it is ignored, the value
+	//  isn't updated. bar is a variable, thus given initial value 'undefined'
+	var foo = 'hi';
+	var bar = function() {
+		return 'nope';
+	};
+
+	// a property foo with reference pointer to this function is created
+	function foo() {
+		return 'yo';
+	}
+
+}());
