20a8a817c8a1a3d1984af65294b05371c8c03404
Kedar <kedardeore@gmail.com>
2017-02-19 15:30:47 +0530

    VueJS - Custom components

--- libraries/vue/app.js
+++ libraries/vue/app.js
@@ -6,6 +6,33 @@ function getRandRgb() {
   return 'rgb(' + randColor() + ', ' + randColor() + ', ' + randColor() + ')';
 }
 
+var num = 3;
+
+// custom component
+Vue.component('my-list', {
+  template: '<ul>' +
+    '<li v-for="item in items">{{ item }}</li>' +
+  '</ul>',
+  /*
+   * the component will accept the following custom attributes
+   * used like this: v-bind:items="some_thing"
+   */
+  props: ['items']
+});
+
+Vue.component('descr', {
+  template: '<p class="small text-muted inline">' +
+    '<mark>#app-{{ num++ }}</mark>' +
+    '<span class="font-italic">({{ csthings }})</span>' +
+  '</p>',
+  props: ['things'],
+  computed: {
+    csthings: function() {
+      return this.things.join(', ');
+    }
+  }
+});
+
 (function() {
   var app = new Vue({
     el: '#app',
@@ -39,7 +66,16 @@ function getRandRgb() {
   var app3 = new Vue({
     el: '#app-3',
     data: {
+      things: ['two-way binding', 'interpolation'],
+      input1: null,
       items: ["Red", "Green", "Blue", "Yellow"]
+    }
+  });
+
+  var app4 = new Vue({
+    el: '#app-4',
+    data: {
+      fruits: ['Mango', 'Orange', 'Pineapple', 'Tomato']
     }
   });
 }());
